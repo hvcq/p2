921bc61c2645bf0fab60eb317175534cda628f13
mellokx <hernanvicentecaro@gmail.com>
2021-10-27 00:46:25 -0300

    terminamos e), b) quedo pendiente con trla, misterio queda por resolver
    Co-authored-by: Alphabeath <Alphabeath@users.noreply.github.com>
    Co-authored-by: vane000 <vane000@users.noreply.github.com>

--- prueba.c
+++ prueba.c
@@ -1,25 +1,67 @@
 #include <stdio.h>
 
-int h(unsigned short x) {
-    short a = x;
-    x = x>>2;
-    short * p = &a;
+/*int h(unsigned short x) {
+    short a = x>>2;
+    short * p = &x;
     short n = x + 4;
-    do {
-        a = a - 1;
+    do{
+        *p -= 1;
         n -= 2;
-    } while (n>4434);
-    return *p*a;
+    }while (n > 0);
+    return n* *p;
+}*/
+
+int h(unsigned short x) {
+    short a = x>>2;
+    short * p = &x;
+    short n = x + 4;
+    do{
+        a -= 1;
+        n -= 2;
+    }while (n > 0);
+    return n*a;
 }
 
 long f(long x, long *ptr) {
     *ptr = x + 1;
-    long result = x % (x+1);
+    long result = x % (*ptr);
+    
     return result;
 }
 
+long fun1(long xx,long yy){
+    return xx + yy;
+}
+
+long fun0(long x, long y){
+    long a = fun1(y,x);
+    long b = fun1(x,y+y);
+    return a + b;
+}  
+
+//Da un ensambladoraceptable y eleva x a n haaaaaaaaaaaaaaa muricion
+// Falta ver manera de ver todas las instrucciones en gdb aunque este con -Og
+int misterio(int x,int n){
+    if(n == 1)
+        return x;
+    else if(n == 0)
+        return 1;
+    int r = misterio(x,n>>1);
+    r = r*r;
+    if(n & 1)
+        r = r*x;
+    return r;
+}
+
+int g(int x, int arr[],int i){
+    int sum = x;
+    sum+= arr[i];
+    return sum; 
+    
+}
 
 int main(){
-    printf("hola\n");
+    int awa = misterio(5,10);
+    printf("%d\n",awa);
     return 0;
 }
